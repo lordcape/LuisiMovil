5ab5edd78617e02585ed9efadb6ae3d6330557ab
SantiagoRodriguez <santiagoelcabe@gmail.com>
2017-06-18 23:07:22 -0300

    funcion comm_test()

---
+++ Arduino/funciones.c
@@ -0,0 +1,26 @@
+/* *** test_comm() *** */
+/* Prueba 5 veces si le llega el comando 0xBA que seria el identificador del master */
+/* Ej:
+    Serial.begin(115200);
+    // master intenta comunicarse con arduino unos 5 intentos
+    if(test_comm()) Serial.println("LUISIMOVIL CONECTADO"); else Serial.println("*** COMM ERROR ***");
+*/
+    
+int test_comm (){
+  
+    int fail = 0;
+    boolean test = 0;
+    byte c;
+    while(fail < 5){
+        while(Serial.available() < 1){}
+        c = Serial.read(); // 0xBA
+	if(c == 0xBA){
+            test = true;
+            break;
+        }
+        else{
+            fail++;
+        }    
+        }
+        if(test) return 1; else return 0;
+}
